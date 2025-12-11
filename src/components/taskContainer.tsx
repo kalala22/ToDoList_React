e0e8eb6da3d3da0ee4f104d52dd14e2c6fc57110
Chretien Kalala <chretienkalala22@gmail.com>
2025-12-11 15:42:06 +0100

    feat: implement task addition functionality in TaskContainer and TaskInput components

--- src/components/taskContainer.tsx
+++ src/components/taskContainer.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import { Footer } from "./footer/footer";
 import { Header } from "./header/header";
 import { TaskInput } from "./taskInput/taskInpunt";
 import { TaskList } from "./taskList/taskList";
+
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 export default function TaskContainer() {
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  const addTask = (title: string) => {
+    const newTask: Task = {
+      id: taskList.length + 1,
+      title,
+      completed: false,
+    };
+    setTaskList([...taskList, newTask]);
+  };
+
   return (
     <main>
       <Header />
-      <TaskInput />
+      <TaskInput addTask={addTask} />
       <TaskList />
       <Footer />
     </main>
--- src/components/taskInput/taskInpunt.tsx
+++ src/components/taskInput/taskInpunt.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import React from "react";
 import styles from "./taskInput.module.css";
-export const TaskInput = () => {
+
+type TaskInputProps = {
+  addTask: (title: string) => void;
+};
+export const TaskInput = ({ addTask }: TaskInputProps) => {
+  const [taskTitle, setTaskTitle] = useState<string>("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskTitle(e.target.value);
+  };
+
+  const handleAddTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (taskTitle.trim()) {
+      addTask(taskTitle);
+      setTaskTitle("");
+    }
+  };
+
   return (
     <div className={`box ${styles.element}`}>
+      addTask
       <h2 className={styles.title}>🎯 Ajoute ta prochaine tache</h2>
-      <form className={styles.container}>
+      <form className={styles.container} onSubmit={handleAddTask}>
         <input
           type="text"
           className={styles.input}
+          value={taskTitle}
+          onChange={handleInputChange}
           placeholder="Indiquez un titre de tache explicite..."
         />
         <button className="button-primary" type="submit">
