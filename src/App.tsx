65790b076994bd041a668c729bdb5e5525b902d9
Chretien Kalala <chretienkalala22@gmail.com>
2025-12-11 12:33:18 +0100

    feat: update styling and structure of the app, add TaskContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
+import "./App.css";
+import TaskContainer from "./components/taskContainer";
 
-import { useState } from 'react'
-import './App.css'
-
-type Priority = "Urgente" | "Moyenne" | "Basse"
-
-type Todo = {
-  id: number
-  text: string
-  priority : Priority
-}
-
-function App() {
-  const [input , setInput] = useState<string>("")
-  const [priority, setPriority] = useState<Priority>("Moyenne")
+export default function App() {
   return (
-    <div className='flex justify-center'>
-      <div className='w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl'>
-      <div className='flex gap-4'>
-        <input type="text"
-        className='input w-full'  
-        placeholder='Ajouter une tâche...'
-        value={input}
-        onChange={(e) => {
-          setInput(e.target.value)
-        }}/>
-        <select className='select w-full'value={priority} onChange={(e) =>{
-          setPriority(e.target.value as Priority )
-        }}>
-          <option value="Urgente">Urgente</option>
-          <option value="Moyenne">Moyenne</option>
-          <option value="Basse">Basse</option>
-        </select>
-        <button className='btn btn-primary'>Ajouter</button>
-      </div>
-      </div>
+    <div>
+      <TaskContainer />
     </div>
-  )
+  );
 }
-
-export default App
