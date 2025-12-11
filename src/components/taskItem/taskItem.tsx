2fed5a680d5f96de5e95b66f6aacb48a1d18d18e
Chretien Kalala <chretienkalala22@gmail.com>
2025-12-11 14:59:25 +0100

    feat: add Footer, TaskItem, and TaskList components with corresponding styles

---
+++ src/components/taskItem/taskItem.tsx
@@ -0,0 +1,15 @@
+import styles from "./taskItem.module.css";
+
+export const TaskItem = () => {
+  return (
+    <li className={`${styles.container} ${styles.default}`}>
+      <div className={styles.item}>
+        <div className={`${styles.id} ${styles.idDefault}`}>1</div>
+        <div className={styles.contentDefault}>
+          Ranger mon bureau et finir d'apprendre React
+        </div>
+      </div>
+      <button className="button-primary">x</button>
+    </li>
+  );
+};
